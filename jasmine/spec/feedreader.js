c9003ca0cf41ec8b4bd21b48dadb26f42724aba7
T-Lab05 <tlab241@gmail.com>
2017-10-09 11:10:58 +0900

    Add test to see the menu icon toggles the feed list

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -67,6 +67,12 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('the menu is toggled when the menu icon is clicked', function(){
+            $('.icon-list').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.icon-list').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+          });
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
